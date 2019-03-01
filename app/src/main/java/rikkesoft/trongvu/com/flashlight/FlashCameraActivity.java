d8ea2e441c83e834980dd34b08e2deae2a15549c
trongvu <vuquoctrong141@gmail.com>
2019-03-01 08:42:05 +0700

    Flash camera 1/3

--- app/src/main/java/rikkesoft/trongvu/com/flashlight/FlashCameraActivity.java
+++ app/src/main/java/rikkesoft/trongvu/com/flashlight/FlashCameraActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ToggleButton;
@@ -14,14 +16,14 @@ import static android.hardware.Camera.Parameters.FLASH_MODE_ON;
 public class FlashCameraActivity extends Activity implements CompoundButton.OnCheckedChangeListener{
 
     private ToggleButton tbtn_turnCamera;
+    private Button btn_click;
     private Camera camera;
     private Camera.Parameters parameters;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcamera);
-        camera = camera.open();
-        parameters = camera.getParameters();
+        getCamera();
         init();
     }
 
@@ -30,6 +32,16 @@ public class FlashCameraActivity extends Activity implements CompoundButton.OnCh
      */
     private void init(){
         tbtn_turnCamera = (ToggleButton)findViewById(R.id.tbtn_turnCamera);
+        btn_click = (Button) findViewById(R.id.btn_click);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(parameters);
+                camera.startPreview();
+            }
+        });
     }
 
     @Override
@@ -41,15 +53,27 @@ public class FlashCameraActivity extends Activity implements CompoundButton.OnCh
         }
     }
 
-    private void offCamera() {
-        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+    private void turnCamera() {
+        parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         camera.setParameters(parameters);
         camera.startPreview();
     }
 
-    private void turnCamera() {
+    private void offCamera() {
+        parameters = camera.getParameters();
         parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
         camera.setParameters(parameters);
         camera.startPreview();
+    }
+
+    /*
+    get Camera
+     */
+    private void getCamera(){
+        if(camera==null&&parameters==null){
+            camera = camera.open();
+            parameters = camera.getParameters();
+        }
     }
 }
